0d84fe71005f7964453af17e5c88a0477ab65e94
JUAN GOMEZ <juandiego.gomez1@utp.edu.co>
2025-12-19 12:52:52 -0500

    Se agrego clerk el cliente de encore

--- authhandler/auth.go
+++ authhandler/auth.go
@@ -3,14 +3,15 @@ package authhandler
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"encore.app/authhandler/sharedauth"
 	"encore.dev/beta/auth"
 	"encore.dev/beta/errs"
 	"encore.dev/types/uuid"
 	"github.com/clerk/clerk-sdk-go/v2"
+	"github.com/clerk/clerk-sdk-go/v2/jwks"
 	"github.com/clerk/clerk-sdk-go/v2/jwt"
+	"github.com/clerk/clerk-sdk-go/v2/user"
 )
 
 var secrets struct {
@@ -18,28 +19,31 @@ var secrets struct {
 }
 
 //encore:service
-type AuthHandler struct{}
+type AuthHandler struct {
+	jwksClient *jwks.Client
+}
 
 func initAuthHandler() (*AuthHandler, error) {
+	if secrets.ClerkSecretKey == "" {
+		return nil, fmt.Errorf("CLERK_SECRET_KEY no está configurada")
+	}
+
 	clerk.SetKey(secrets.ClerkSecretKey)
-	return &AuthHandler{}, nil
+
+	config := &clerk.ClientConfig{}
+	config.Key = clerk.String(secrets.ClerkSecretKey)
+
+	jwksClient := jwks.NewClient(config)
+	return &AuthHandler{jwksClient: jwksClient}, nil
 }
 
 //encore:authhandler
-func (s *AuthHandler) AuthHandler(ctx context.Context, p *MyAuthParams) (auth.UID, *AuthData, error) {
-	// Extract the token from the request
-	token, err := extractToken(p)
-	if err != nil {
-		return "", nil, err
-	}
+func (s *AuthHandler) AuthHandler(ctx context.Context, p *Params) (auth.UID, *AuthData, error) {
 
-	fmt.Println("Token: ", token)
+	// %+v prints with field names: {Name:Arun Age:25}
+	userSub, err := verifySessionToken(ctx, p.Token, s.jwksClient)
 
-	claims, err := jwt.Verify(ctx, &jwt.VerifyParams{
-		Token: token,
-	})
-
-	fmt.Println("claims: ", claims, "err: ", err)
+	fmt.Println("user: ", userSub, "err: ", err)
 
 	if err != nil {
 		return "", nil, &errs.Error{
@@ -50,6 +54,7 @@ func (s *AuthHandler) AuthHandler(ctx context.Context, p *MyAuthParams) (auth.UI
 
 	}
 
+	// Este error es controlado, ignorar esto. No se ha implementado toda la logica por eso se lanza error
 	return "", nil, &errs.Error{
 		Code:    errs.Unauthenticated,
 		Message: "Error al autenticar el usuario. ",
@@ -57,9 +62,10 @@ func (s *AuthHandler) AuthHandler(ctx context.Context, p *MyAuthParams) (auth.UI
 	}
 }
 
-type MyAuthParams struct {
+type Params struct {
+	Token string `header:"authorization"`
 	// Extract the authorization header
-	AuthorizationHeader string `header:"Authorization"`
+	MembershipIdSelected string `header:"X-Membership-ID"`
 }
 
 type AuthData struct {
@@ -68,26 +74,53 @@ type AuthData struct {
 	MembershipID uuid.UUID
 }
 
-// Extract token from AuthParams
-func extractToken(p *MyAuthParams) (token string, err error) {
-	if p.AuthorizationHeader == "" {
-		fmt.Errorf("No authentication token provided")
+func verifySessionToken(ctx context.Context, sessionToken string, jwksClient *jwks.Client) (string, error) {
+	unsafeClaims, err := jwt.Decode(ctx, &jwt.DecodeParams{
+		Token: sessionToken,
+	})
+	if err != nil {
 		return "", &errs.Error{
 			Code:    errs.Unauthenticated,
-			Message: "Error al autenticar el usuario.",
+			Message: "Error al decodificar el token de sesión.",
 		}
 	}
 
-	if after, found := strings.CutPrefix(p.AuthorizationHeader, "Bearer "); found {
-		token = strings.TrimSpace(after)
-		if token != "" {
-			return token, nil
+	jwk, err := jwt.GetJSONWebKey(ctx, &jwt.GetJSONWebKeyParams{
+		KeyID:      unsafeClaims.KeyID,
+		JWKSClient: jwksClient,
+	})
+	if err != nil {
+		return "", &errs.Error{
+			Code:    errs.Unauthenticated,
+			Message: "1- Error al obtener la clave pública para verificar el token de sesión.",
 		}
 	}
-	return "", &errs.Error{
-		Code:    errs.Unauthenticated,
-		Message: "Formato de header de autorizacion invalido",
-		Details: sharedauth.ErrorDetailsToken{TokenStatus: "invalid_token"},
+
+	claims, err := jwt.Verify(ctx, &jwt.VerifyParams{
+		Token: sessionToken,
+		JWK:   jwk,
+	})
+
+	if err != nil {
+		return "", &errs.Error{
+			Code:    errs.Unauthenticated,
+			Message: "2-Error al obtener la clave pública para verificar el token de sesión.",
+		}
 	}
+
+	usr, err := user.Get(ctx, claims.Subject)
+	if err != nil {
+		return "", &errs.Error{
+			Code:    errs.Unauthenticated,
+			Message: "Error al obtener el usuario desde Clerk.",
+		}
+	}
+
+	fmt.Println("user first name: ", *usr.FirstName)
+	fmt.Println("user last name: ", usr.LastName)
+
+	fmt.Println("user email: ", usr.EmailAddresses[0].EmailAddress)
+
+	return usr.ID, nil
 
 }
--- encore.app
+++ encore.app
@@ -1,3 +1,17 @@
 {
 	"id": "encoretruck-zpoi",
+	
+
+	"global_cors": {
+		"debug": true,
+		"allow_headers": ["Authorization", "X-Membership-Id"],
+    "allow_origins_without_credentials": [
+      "http://192.168.20.23:5173",
+      "http://localhost:5173"
+    ],
+    "allow_origins_with_credentials": [
+      "http://192.168.20.23:5173",
+      "http://localhost:5173"
+    ]
+  }
 }
